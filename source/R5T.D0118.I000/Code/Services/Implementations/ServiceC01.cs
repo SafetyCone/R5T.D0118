e099f497b14e330266b73cd6f5336a5d8fc97cf3
davidcoats <david@getsafetycone.com>
2022-11-18 17:47:14 -0800

    Interim changes.

---
+++ source/R5T.D0118.I000/Code/Services/Implementations/ServiceC01.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using R5T.T0064;
+
+
+namespace R5T.D0118.I000
+{
+    [ServiceImplementationMarker]
+    public class ServiceC01 : IServiceC, IServiceImplementation
+    {
+        public async Task RunAction()
+        {
+            // Take some time, simulating doing something.
+            await Task.Delay(3000);
+        }
+
+        public async Task<string> RunFunction()
+        {
+            // Take some time, simulating doing something.
+            await Task.Delay(3000);
+
+            // Then return something.
+            var output = $"{nameof(ServiceC01)} result";
+            return output;
+        }
+    }
+}
---
+++ source/R5T.D0118/Code/Services/Definitions/IServiceC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using R5T.T0064;
+
+
+namespace R5T.D0118
+{
+    /// <summary>
+    /// An asynchronous service.
+    /// </summary>
+    [ServiceDefinitionMarker]
+    public interface IServiceC : IServiceDefinition
+    {
+        Task RunAction();
+        Task<string> RunFunction();
+    }
+}
